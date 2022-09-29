fb90932a82af169fdd08dabebcfea6477e6ce6cf
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-09-29 21:48:17 +0200

    models fixes

--- ZeroWaste/Models/Photo.cs
+++ ZeroWaste/Models/Photo.cs
@@ -2,14 +2,18 @@
 
 namespace ZeroWaste.Models
 {
-    public class RecipePhoto
+    public class Photo
     {
         public int Id { get; set; }
         public string PhotoBinary { get; set; }
         public DateTime CreatedAt { get; set; }
 
-        public int RecipeReviewId { get; set; }
+        public int? RecipeReviewId { get; set; }
         [ForeignKey(nameof(RecipeReviewId))]
-        public RecipeReview RecipeReview { get; set; }
+        public RecipeReview? RecipeReview { get; set; }
+
+        public int? RecipeId { get; set; }
+        [ForeignKey(nameof(RecipeId))]
+        public Recipe? Recipe { get; set; }
     }
 }
