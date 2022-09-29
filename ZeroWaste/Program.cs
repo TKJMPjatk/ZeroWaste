cb1124e3b579277126781c1d3c2b72d1e50ec660
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-09-29 21:49:42 +0200

    added method for seeding database
    in progress...

--- ZeroWaste/Program.cs
+++ ZeroWaste/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(buil
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -16,6 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+AppDbInitializer.SeedRolesAndUsersAsync(app).Wait();
+AppDbInitializer.Seed(app);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -29,3 +33,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
