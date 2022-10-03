dfae42024b9e396126ca59d999c969725934c441
s19424 <s19424@pjwstk.edu.pl>
2022-10-03 19:36:55 +0200

    Added Category Service with interface. Added View to search by categories

--- ZeroWaste/Controllers/SearchRecipesController.cs
+++ ZeroWaste/Controllers/SearchRecipesController.cs
@@ -1,15 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using ZeroWaste.Data.Helpers;
+using ZeroWaste.Data.Services;
 using ZeroWaste.Data.ViewModels.Recipes;
+using ZeroWaste.Models;
 
 namespace ZeroWaste.Controllers;
 
 public class SearchRecipesController : Controller
 {
     private readonly IUrlQueryHelper _urlQueryHelper;
-    public SearchRecipesController(IUrlQueryHelper urlQueryHelper)
+    private readonly ICategoryService _categoryService;
+    public SearchRecipesController(IUrlQueryHelper urlQueryHelper, ICategoryService categoryService)
     {
         _urlQueryHelper = urlQueryHelper;
+        _categoryService = categoryService;
     }   
     public IActionResult SearchByIngredients()
     {
@@ -21,5 +25,10 @@ public class SearchRecipesController : Controller
         if(ingredientsVm != null)
             ingredientsForSearchList = _urlQueryHelper.GetIngredientsFromUrl(ingredientsVm);
         return View();
+    }
+    public async Task<IActionResult> SearchByCategories()
+    {
+        List<Category> categories = await _categoryService.GetAllAsync();
+        return View(categories);
     }
 }
---
+++ ZeroWaste/Data/ViewModels/Recipes/RecipeResultByIngredientsVM.cs
@@ -0,0 +1,8 @@
+using ZeroWaste.Models;
+
+namespace ZeroWaste.Data.ViewModels.Recipes;
+
+public class RecipeResultByIngredientsVM
+{
+    public List<Recipe> Recipes { get; set; }
+}
