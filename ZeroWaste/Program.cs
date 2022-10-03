1cb30d2e38ba40e318a30fadc5ffc414aad5b328
s19424 <s19424@pjwstk.edu.pl>
2022-10-03 19:37:19 +0200

    Added DI in program.cs

--- ZeroWaste/Program.cs
+++ ZeroWaste/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ZeroWaste.Data;
 using ZeroWaste.Data.Helpers;
+using ZeroWaste.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddScoped<IUrlQueryHelper, UrlQueryHelper>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
