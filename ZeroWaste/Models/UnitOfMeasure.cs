56c0e6d5cc1e279abfc7edfb90d065aaff11e40a
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-10-04 19:03:08 +0200

    ingredient list in progress...

--- ZeroWaste/Models/UnitOfMeasure.cs
+++ ZeroWaste/Models/UnitOfMeasure.cs
@@ -1,9 +1,13 @@
-﻿namespace ZeroWaste.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ZeroWaste.Models
 {
     public class UnitOfMeasure
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [Display(Name = "Jednostka miary")]
         public string Shortcut { get; set; }
 
         public List<Ingredient> Ingredients { get; set; }
