65d327987f6bb0caab7a77b55fd4b1f3091f6ad4
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-10-04 20:17:43 +0200

    Added ingredient service

---
+++ ZeroWaste/Data/Services/IngredientsService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZeroWaste.Models;
+
+namespace ZeroWaste.Data.Services
+{
+    public class IngredientsService : IIngredientsService
+    {
+        private readonly AppDbContext _context;
+
+        public IngredientsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Ingredient>> GetAllAsync()
+        {
+            return await _context.Ingredients
+                .Include(i => i.IngredientType)
+                .Include(i => i.UnitOfMeasure)
+                .ToListAsync();
+        }
+
+        public async Task<List<Ingredient>> GetAllAsync(string searchString)
+        {
+            return await _context.Ingredients
+                .Where(i => i.Name.Contains(searchString))
+                .Include(i => i.IngredientType)
+                .Include(i => i.UnitOfMeasure)
+                .ToListAsync();
+        }
+    }
+}
