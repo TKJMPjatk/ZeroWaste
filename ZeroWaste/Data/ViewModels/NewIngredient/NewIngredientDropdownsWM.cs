1623a768968a146400e010b56e535d69509c02ca
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-10-04 21:44:30 +0200

    created models for new ingredient form

---
+++ ZeroWaste/Data/ViewModels/NewIngredient/NewIngredientDropdownsWM.cs
@@ -0,0 +1,10 @@
+﻿using ZeroWaste.Models;
+
+namespace ZeroWaste.Data.ViewModels.NewIngredient
+{
+    public class NewIngredientDropdownsWM
+    {
+        public List<IngredientType> IngredientTypes { get; set; }
+        public List<UnitOfMeasure> UnitOfMeasures { get; set; }
+    }
+}
