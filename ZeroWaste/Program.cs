f6f64a973fc3c4b0dd43e9031d8fa70d14849df5
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-10-05 21:45:51 +0200

    added services

--- ZeroWaste/Program.cs
+++ ZeroWaste/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddScoped<IUrlQueryHelper, UrlQueryHelper>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IShoppingListsService, ShoppingListsService>();
 builder.Services.AddScoped<IIngredientsService, IngredientsService>();
+builder.Services.AddScoped<IIngredientMapperHelper, IngredientMapperHelper>();
 var app = builder.Build();
 
 
