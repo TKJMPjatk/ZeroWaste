65131d968dc7c0093db92986661e73ab5ba5a14f
s19424 <s19424@pjwstk.edu.pl>
2022-10-06 18:44:35 +0200

    Added NewShoppingListVM

---
+++ ZeroWaste/Data/ViewModels/ShoppingList/NewShoppingListVM.cs
@@ -0,0 +1,8 @@
+namespace ZeroWaste.Data.ViewModels.ShoppingList;
+
+public class NewShoppingListVM
+{
+    public string Title { get; set; }
+    public string Note { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
