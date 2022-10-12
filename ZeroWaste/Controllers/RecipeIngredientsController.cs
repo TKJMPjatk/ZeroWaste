f18902a70651df29379ca7168beeac81cb8c02e6
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-10-12 21:31:48 +0200

    beginning of work on the recipe ingredients form

---
+++ ZeroWaste/Controllers/RecipeIngredientsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ZeroWaste.Data.Services.RecipeIngredients;
+
+namespace ZeroWaste.Controllers
+{
+    public class RecipeIngredientsController : Controller
+    {
+        private readonly IRecipeIngredientService _service;
+
+        public RecipeIngredientsController(IRecipeIngredientService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> AddIngredients(int? recipeId)
+        {
+            var recipeDropdownsData = await _service.GetDropdownsValuesAsync();
+            ViewBag.Ingredients = new SelectList(recipeDropdownsData.Ingredients, "Id", "Name");
+            ViewBag.UnitOfMeasures = new SelectList(recipeDropdownsData.UnitOfMeasures, "Id", "Name");
+
+            return View();
+        }
+    }
+}
---
+++ ZeroWaste/Data/Services/RecipeIngredients/IRecipeIngredientService.cs
@@ -0,0 +1,11 @@
+﻿using ZeroWaste.Data.ViewModels.RecipeIngredients;
+using ZeroWaste.Models;
+
+namespace ZeroWaste.Data.Services.RecipeIngredients
+{
+    public interface IRecipeIngredientService
+    {
+        Task<RecipeIngredientsDropdownsVM> GetDropdownsValuesAsync();
+        Task<IEnumerable<RecipeIngredient>> GetCurrentIngredientsAsync(int? recipeId);
+    }
+}
