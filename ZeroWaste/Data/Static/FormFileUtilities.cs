ace24c1b93a58460e853962c05a132e43e4e08f3
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-10-14 21:30:47 +0200

    create formfileutilites class

---
+++ ZeroWaste/Data/Static/FormFileUtilities.cs
@@ -0,0 +1,12 @@
+﻿namespace ZeroWaste.Data.Static
+{
+    public static class FormFileUtilities
+    {
+        public static async Task<byte[]> GetBytes(this IFormFile formFile)
+        {
+            await using MemoryStream memoryStream = new();
+            await formFile.CopyToAsync(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
