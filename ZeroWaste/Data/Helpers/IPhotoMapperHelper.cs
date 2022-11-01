1fd3d9a550b39622445a19457ff933a0608bc039
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-11-01 12:07:30 +0100

    created mapper helper photos

---
+++ ZeroWaste/Data/Helpers/IPhotoMapperHelper.cs
@@ -0,0 +1,10 @@
+﻿using ZeroWaste.Data.ViewModels.ExistingRecipe;
+using ZeroWaste.Models;
+
+namespace ZeroWaste.Data.Helpers
+{
+    public interface IPhotoMapperHelper
+    {
+        IEnumerable<PhotoVM> Map(IEnumerable<Photo> photos);
+    }
+}
---
+++ ZeroWaste/Data/Helpers/PhotoMapperHelper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ZeroWaste.Data.ViewModels.ExistingRecipe;
+using ZeroWaste.Models;
+
+namespace ZeroWaste.Data.Helpers
+{
+    public class PhotoMapperHelper : IPhotoMapperHelper
+    {
+        private readonly IMapper _mapper;
+        public PhotoMapperHelper(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public IEnumerable<PhotoVM> Map(IEnumerable<Photo> photos)
+        {
+            var photosMapped = _mapper.Map<IEnumerable<PhotoVM>>(photos);
+            return photosMapped;
+        }
+    }
+}
