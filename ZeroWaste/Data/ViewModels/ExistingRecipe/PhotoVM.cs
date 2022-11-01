0b4ea49d54e1b8b7b36944cab3d0973452757c59
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-11-01 12:06:57 +0100

    create photo vm class

---
+++ ZeroWaste/Data/ViewModels/ExistingRecipe/PhotoVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ZeroWaste.Data.ViewModels.ExistingRecipe
+{
+    public class PhotoVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] BinaryPhoto { get; set; }
+    }
+}
