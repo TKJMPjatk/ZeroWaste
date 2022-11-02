3b304187d9a18dacb00ffa3b680ac8a8a884f400
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-11-02 19:08:59 +0100

    created identity error code to polish method

---
+++ ZeroWaste/Data/Static/IdentityErrorPolishTranslation.cs
@@ -0,0 +1,26 @@
+﻿namespace ZeroWaste.Data.Static
+{
+    public static class IdentityErrorPolishTranslation
+    {
+        public static string TranslateErrorCodeToPolish(this string Code)
+        {
+            switch (Code)
+            {
+                case "PasswordToShort":
+                    return "Hasło musi składać się conajmniej z 6 znaków.";
+                case "PasswordRequiresNonAlphanumeric":
+                    return "Hasło musi zawierać conajmniej jeden znak specjalny.";
+                case "PasswordRequiresDigit":
+                    return "Haslo musi zawierać conajmniej jedną cyfrę.";
+                case "PasswordMismatch":
+                    return "Obecne hasło jest nieprawidłowe.";
+                case "PasswordRequiresUpper":
+                    return "Hasło musi zawierać conajmniej jedną wielką literę.";
+                case "PasswordRequiresLower":
+                    return "Haslo musi zawierać conajmniej jedną małą literę.";
+                default:
+                    return Code;
+            }
+        }
+    }
+}
