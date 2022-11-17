07d13242548912016f1bd901a4cab0a76c30e0af
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-11-17 19:41:44 +0100

    created harmfulingredientsservice

---
+++ ZeroWaste/Data/Services/HarmfulIngredients/IHarmfulIngredientsService.cs
@@ -0,0 +1,12 @@
+﻿using ZeroWaste.Data.ViewModels;
+
+namespace ZeroWaste.Data.Services.HarmfulIngredients
+{
+    public interface IHarmfulIngredientsService
+    {
+        Task<IEnumerable<HarmfulIngredientVM>> GetHarmfulIngredientsForUser(string userId);
+        Task<IEnumerable<HarmfulIngredientVM>> GetSafeIngredientsForUser(string userId);
+        Task MarkIngredientAsHarmful(string userId, int ingredientId);
+        Task UnmarkIngredientAsHarmful(string userId, int ingredientId);
+    }
+}
