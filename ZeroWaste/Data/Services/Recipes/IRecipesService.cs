3509733b0101cf6f7dcc299710e8224da86e0d78
s19424 <s19424@pjwstk.edu.pl>
2022-11-24 22:04:17 +0100

    Fix bugs

--- ZeroWaste/Data/Services/Recipes/IRecipesService.cs
+++ ZeroWaste/Data/Services/Recipes/IRecipesService.cs
@@ -19,6 +19,7 @@ namespace ZeroWaste.Data.Services.Recipes
         Task<bool> IsAuthorEqualsEditor(int recipeId, string editorId);
         Task<List<int>> GetRecipeIdList();
         Task ConfirmRecipe(int recipeId);
+        Task RejectRecipe(int recipeId);
         Task UpdateStateAsync(int recipeId, int statusId);
     }
 }
