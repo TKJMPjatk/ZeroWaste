644f270d883c6ea566077b6744f9b50582a2e238
s19424 <s19424@pjwstk.edu.pl>
2022-12-19 20:52:47 +0100

    Added first integration tests

--- ZeroWaste.IntegrationTests/ShoppingListHandlerTests.cs
+++ ZeroWaste.IntegrationTests/ShoppingListHandlerTests.cs
@@ -1,12 +1,16 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ZeroWaste.Data;
 using ZeroWaste.Data.Handlers.ShoppingListHandlers;
 using ZeroWaste.Data.Services.ShoppingLists;
 using ZeroWaste.IntegrationTests.Helpers;
+using ZeroWaste.Models;
 
 namespace ZeroWaste.IntegrationTests;
 
@@ -14,7 +18,8 @@ public class ShoppingListHandlerTests : IClassFixture<WebApplicationFactory<Prog
 {
     private readonly string _connectionString;
     private WebApplicationFactory<Program> _factory;
-    IShoppingListHandler shoppingListsService;
+    IShoppingListHandler _shoppingListHandler;
+    private AppDbContext _dbContext;
     public ShoppingListHandlerTests(WebApplicationFactory<Program> factory)
     {
         IConfiguration configuration = new ConfigurationBuilder()
@@ -31,19 +36,70 @@ public class ShoppingListHandlerTests : IClassFixture<WebApplicationFactory<Prog
                     );
                     services.Remove(dbContextOptions);
                     services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_connectionString));
-                    services.AddScoped<IShoppingListHandler, ShoppingListHandler>();
-                    var tmp = services.BuildServiceProvider();
-                    shoppingListsService = tmp.GetService<IShoppingListHandler>();
                 });
-                
             });
+        var serviceScope = _factory.Services.CreateScope();
+        _shoppingListHandler = serviceScope.ServiceProvider.GetService<IShoppingListHandler>();
+        _dbContext = serviceScope.ServiceProvider.GetService<AppDbContext>();
     }
 
     [Fact]
-    public async Task Testing()
+    public async Task GetShoppingListById_forExistingShoppingList_ShouldReturnShoppingList()
     {
-        var tmp = _connectionString;
-        var tmp2 = shoppingListsService;
-        Assert.Equal(_connectionString, _connectionString);
+        //Arrange
+        var existedShoppingList = await SeedShoppingList(_dbContext);
+        //Act
+        var shoppingList = await _shoppingListHandler.GetShoppingListById(existedShoppingList.Id);
+        //Assert
+        Assert.Equal(existedShoppingList.Id, shoppingList.Id);
+        CleanDb.Clean(_connectionString);
     }
+    private async Task<ShoppingList> SeedShoppingList(AppDbContext context)
+    {
+        var shopppingList = new ShoppingList()
+        {
+            Note = Guid.NewGuid().ToString(),
+            Title = Guid.NewGuid().ToString(),
+            CreatedAt = System.DateTime.Now,
+            UserId = "0d442e09-d1a4-43ea-b1da-a60a7cd0c6a0"
+        };
+        await context.ShoppingLists.AddAsync(shopppingList);
+        await context.SaveChangesAsync();
+        return shopppingList;
+    }
+    public async Task GetShoppingListByUserId_ForExistingShoppingLists_ShouldReturnShoppingList()
+    {
+        //Arrange
+        var userId = await GetUserGuid();
+        var existedShoppingList = await SeedShoppingList(_dbContext);
+        //Act 
+        var shoppingList = await _shoppingListHandler.GetShoppingListsByUserId()
+    }
+
+    private async Task<string> GetUserGuid()
+    {
+        var userGuid = await _dbContext.Users.Select(x => x.Id).FirstOrDefaultAsync();
+        return userGuid;
+    }
+    private async Task<ShoppingList> SeedShoppingList(AppDbContext context, string userId)
+    {
+        var shopppingList = new ShoppingList()
+        {
+            Note = Guid.NewGuid().ToString(),
+            Title = Guid.NewGuid().ToString(),
+            CreatedAt = System.DateTime.Now,
+            UserId = userId
+        };
+        var shopppingList1 = new ShoppingList()
+        {
+            Note = Guid.NewGuid().ToString(),
+            Title = Guid.NewGuid().ToString(),
+            CreatedAt = System.DateTime.Now,
+            UserId = userId
+        };
+        await context.ShoppingLists.AddAsync(shopppingList1);
+        await context.SaveChangesAsync();
+        return shopppingList;
+    }
+
 }
