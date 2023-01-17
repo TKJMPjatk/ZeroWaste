0114f63417ddfce5fbe9ead3a36cedcd5a5885ac
s19424 <s19424@pjwstk.edu.pl>
2023-01-17 21:16:28 +0100

    no message

--- ZeroWaste/Controllers/SearchRecipesController.cs
+++ ZeroWaste/Controllers/SearchRecipesController.cs
@@ -244,7 +244,7 @@ public class SearchRecipesController : Controller
         //ViewBag.SortTypes = Enum.GetValues(typeof(SortTypes)).Cast<SortTypes>().ToList();
         ViewBag.SortTypes = _sortTypeDisplayVmList;
         ViewBag.Statuses = await _statusesService.GetAllAsync();
-        ViewBag.PageTitle = "Nie ulubione przepisy";
+        ViewBag.PageTitle = "Nieulubione przepisy";
         var resultsVm = await _searchRecipeContext.GetSearchRecipeResultVm(new SearchRecipeResultsVm()
         {
             UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value,
